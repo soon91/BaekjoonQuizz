faead4e4c9483aa990a585af612bc48d8acc9d28
soon91 <135226621+soon91@users.noreply.github.com>
2023-06-18 01:08:59 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 328.08 ms, Memory: 94.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -9,7 +9,7 @@ class Solution {
         for (int i = 0; i < completion.length; i++) {
             if(!participant[i].equals(completion[i])){
                 result = participant[i];
-                break; // 효율성을 위해 break 추가
+                break;
             }
         }
         if(result.equals("")) result = participant[participant.length - 1];

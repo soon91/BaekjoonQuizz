cf1e503e5412571a9b0050e646e48bac799e3902
soon91 <135226621+soon91@users.noreply.github.com>
2023-06-20 19:25:19 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.24 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(long n) {
+        int answer = 0;
+        
+        long ternary = 0;
+        String strTer = "";
+
+        long x = 1;
+        while (n > 0) {
+            ternary += (n % 3)*x;
+            n /= 3;
+            x *=10;
+        }
+
+        strTer = Long.toString(ternary);
+        String[] arrStr = strTer.split("");
+        
+        for (int i = 0; i < strTer.length(); i++) {
+            answer += Integer.parseInt(arrStr[i]) * Math.pow(3,i);
+        }
+        
+        return answer;
+    }
+}

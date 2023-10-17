94d3e8f45bb70bcc282b326f47bce3f56a77adba
Soon Choi <135226621+soon91@users.noreply.github.com>
2023-10-17 21:41:01 +0900

    [level unrated] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/unrated/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
+++ 프로그래머스/unrated/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -1,13 +1,12 @@
 class Solution {
     public int[] solution(int[] array) {
         int[] answer = new int[2];
-        int index = 0;
 
         for (int i = 0; i < array.length; i++) {
             if (i == 0) {
                 answer[0] = array[0];
                 answer[1] = i;
-            } else if (array[i] > answer[index]) {
+            } else if (array[i] > answer[0]) {
                 answer[0] = array[i];
                 answer[1] = i;
             }

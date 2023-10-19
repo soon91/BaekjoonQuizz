ee63e4ad19ff3b5d96bbe64d1d65ddd07671195e
Soon Choi <135226621+soon91@users.noreply.github.com>
2023-10-20 01:00:09 +0900

    [level unrated] Title: 카운트 다운, Time: 0.02 ms, Memory: 71.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int start, int end_num) {
+        int[] answer = new int[start-end_num+1];
+        
+        int index = 0;
+        for (int i = start; i >= end_num; i--) {
+            answer[index] = i;
+            index++;
+        }
+        
+        return answer;
+    }
+}

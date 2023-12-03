bec4c5ce81d1014bc9eaa3c1ecdb45470f3ff05a
Soon Choi <135226621+soon91@users.noreply.github.com>
2023-12-04 05:03:33 +0900

    [level unrated] Title: 9로 나눈 나머지, Time: 0.03 ms, Memory: 71.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(String number) {
+        int answer = 0;
+        
+        
+        for (int i = 0; i < number.length(); i++) {
+            answer += Character.getNumericValue(number.charAt(i));
+        }
+        
+        answer = answer % 9;
+        
+        return answer;
+    }
+}

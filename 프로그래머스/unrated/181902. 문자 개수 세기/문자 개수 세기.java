a44522395d2070db4cd9fbc2c3209b4514d79e16
Soon Choi <135226621+soon91@users.noreply.github.com>
2023-12-17 18:36:48 +0900

    [level unrated] Title: 문자 개수 세기, Time: 0.55 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.java
@@ -0,0 +1,115 @@
+class Solution {
+    public int[] solution(String my_string) {
+        int[] answer = new int[52];
+
+        for (int i = 0; i < my_string.length(); i++) {
+            if (my_string.split("")[i].equals("A")) {
+                answer[0]++;
+            } else if (my_string.split("")[i].equals("B")) {
+                answer[1]++;
+            } else if (my_string.split("")[i].equals("C")) {
+                answer[2]++;
+            } else if (my_string.split("")[i].equals("D")) {
+                answer[3]++;
+            } else if (my_string.split("")[i].equals("E")) {
+                answer[4]++;
+            } else if (my_string.split("")[i].equals("F")) {
+                answer[5]++;
+            } else if (my_string.split("")[i].equals("G")) {
+                answer[6]++;
+            } else if (my_string.split("")[i].equals("H")) {
+                answer[7]++;
+            } else if (my_string.split("")[i].equals("I")) {
+                answer[8]++;
+            } else if (my_string.split("")[i].equals("J")) {
+                answer[9]++;
+            } else if (my_string.split("")[i].equals("K")) {
+                answer[10]++;
+            } else if (my_string.split("")[i].equals("L")) {
+                answer[11]++;
+            } else if (my_string.split("")[i].equals("M")) {
+                answer[12]++;
+            } else if (my_string.split("")[i].equals("N")) {
+                answer[13]++;
+            } else if (my_string.split("")[i].equals("O")) {
+                answer[14]++;
+            } else if (my_string.split("")[i].equals("P")) {
+                answer[15]++;
+            } else if (my_string.split("")[i].equals("Q")) {
+                answer[16]++;
+            } else if (my_string.split("")[i].equals("R")) {
+                answer[17]++;
+            } else if (my_string.split("")[i].equals("S")) {
+                answer[18]++;
+            } else if (my_string.split("")[i].equals("T")) {
+                answer[19]++;
+            } else if (my_string.split("")[i].equals("U")) {
+                answer[20]++;
+            } else if (my_string.split("")[i].equals("V")) {
+                answer[21]++;
+            } else if (my_string.split("")[i].equals("W")) {
+                answer[22]++;
+            } else if (my_string.split("")[i].equals("X")) {
+                answer[23]++;
+            } else if (my_string.split("")[i].equals("Y")) {
+                answer[24]++;
+            } else if (my_string.split("")[i].equals("Z")) {
+                answer[25]++;
+            } else if (my_string.split("")[i].equals("a")) {
+                answer[26]++;
+            } else if (my_string.split("")[i].equals("b")) {
+                answer[27]++;
+            } else if (my_string.split("")[i].equals("c")) {
+                answer[28]++;
+            } else if (my_string.split("")[i].equals("d")) {
+                answer[29]++;
+            } else if (my_string.split("")[i].equals("e")) {
+                answer[30]++;
+            } else if (my_string.split("")[i].equals("f")) {
+                answer[31]++;
+            } else if (my_string.split("")[i].equals("g")) {
+                answer[32]++;
+            } else if (my_string.split("")[i].equals("h")) {
+                answer[33]++;
+            } else if (my_string.split("")[i].equals("i")) {
+                answer[34]++;
+            } else if (my_string.split("")[i].equals("j")) {
+                answer[35]++;
+            } else if (my_string.split("")[i].equals("k")) {
+                answer[36]++;
+            } else if (my_string.split("")[i].equals("l")) {
+                answer[37]++;
+            } else if (my_string.split("")[i].equals("m")) {
+                answer[38]++;
+            } else if (my_string.split("")[i].equals("n")) {
+                answer[39]++;
+            } else if (my_string.split("")[i].equals("o")) {
+                answer[40]++;
+            } else if (my_string.split("")[i].equals("p")) {
+                answer[41]++;
+            } else if (my_string.split("")[i].equals("q")) {
+                answer[42]++;
+            } else if (my_string.split("")[i].equals("r")) {
+                answer[43]++;
+            } else if (my_string.split("")[i].equals("s")) {
+                answer[44]++;
+            } else if (my_string.split("")[i].equals("t")) {
+                answer[45]++;
+            } else if (my_string.split("")[i].equals("u")) {
+                answer[46]++;
+            } else if (my_string.split("")[i].equals("v")) {
+                answer[47]++;
+            } else if (my_string.split("")[i].equals("w")) {
+                answer[48]++;
+            } else if (my_string.split("")[i].equals("x")) {
+                answer[49]++;
+            } else if (my_string.split("")[i].equals("y")) {
+                answer[50]++;
+            } else if (my_string.split("")[i].equals("z")) {
+                answer[51]++;
+            }
+        }
+        
+        return answer;
+    }
+}

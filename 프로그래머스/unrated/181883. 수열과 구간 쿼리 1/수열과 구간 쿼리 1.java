eff08a8983682237ad1fe7ae2d0968a6b01fd93f
Soon Choi <135226621+soon91@users.noreply.github.com>
2023-12-29 03:33:49 +0900

    [level unrated] Title: 수열과 구간 쿼리 1, Time: 1.63 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = arr;
+
+        for (int i = 0; i < queries.length; i++) {
+            for (int j = queries[i][0]; j <= queries[i][1]; j++) {
+                answer[j] = answer[j] + 1;
+            }
+        }
+        
+        return answer;
+    }
+}

f0dcd92796d63e120a24377c12a74a06c1868063
Soon Choi <135226621+soon91@users.noreply.github.com>
2024-01-03 02:31:24 +0900

    [level unrated] Title: 특별한 이차원 배열 1, Time: 0.14 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[][] solution(int n) {
+        int[][] answer = new int[n][n];
+
+        for (int i = 0; i < answer.length; i++) {
+            for (int j = 0; j < answer.length; j++) {
+                if (i == j) {
+                    answer[i][j] = 1;
+                } else {
+                    answer[i][j] = 0;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}

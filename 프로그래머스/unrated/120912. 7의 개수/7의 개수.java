1bcc9ebeb7b10d90613536b0a89b4ecba370fcd4
Soon Choi <135226621+soon91@users.noreply.github.com>
2024-01-10 02:32:47 +0900

    [level unrated] Title: 7의 개수, Time: 0.06 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < String.valueOf(array[i]).length(); j++) {
+                if (String.valueOf(array[i]).charAt(j) == '7') {
+                    answer++;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}

43990e665ded551cb8e63f2e946cbb70e4a39329
Soon Choi <135226621+soon91@users.noreply.github.com>
2024-01-21 00:16:49 +0900

    [level 0] Title: 커피 심부름, Time: 0.02 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -3,9 +3,7 @@ class Solution {
         int answer = 0;
         
         for (int i = 0; i < order.length; i++) {
-            if (order[i].equals("icecafelatte") || order[i].equals("cafelatteice")
-                    || order[i].equals("hotcafelatte") || order[i].equals("cafelattehot")
-                    || order[i].equals("cafelatte") ) {
+            if (order[i].contains("cafelatte") ) {
                 answer += 5000;
             } else {
                 answer += 4500;

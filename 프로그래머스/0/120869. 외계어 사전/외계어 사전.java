e38e467598fb9ef2ace752de0186f11de47791cc
Soon Choi <135226621+soon91@users.noreply.github.com>
2024-01-25 01:20:59 +0900

    [level 0] Title: 외계어 사전, Time: 0.05 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(String[] spell, String[] dic) {
+        int answer = 2;
+
+        for (int i = 0; i < dic.length; i++) {
+            boolean chack = true;
+            
+            for (int j = 0; j < spell.length; j++) {
+                if (!dic[i].contains(spell[j])) {
+                    chack = false;
+                    break;
+                }
+            }
+            
+            if (chack) {
+                answer = 1;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}

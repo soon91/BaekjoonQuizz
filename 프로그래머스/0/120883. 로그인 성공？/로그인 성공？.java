0aa35391ea9ebc8c4bed79ebac266fe2d8e1e0e3
Soon Choi <135226621+soon91@users.noreply.github.com>
2024-01-26 00:32:15 +0900

    [level 0] Title: 로그인 성공?, Time: 0.01 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(String[] id_pw, String[][] db) {
+        String answer = "";
+        
+        for (int i = 0; i < db.length; i++) {
+            if (db[i][0].equals(id_pw[0]) && db[i][1].equals(id_pw[1])) {
+                answer = "login";
+                break;
+            } else if (db[i][0].equals(id_pw[0]) && !db[i][1].equals(id_pw[1])) {
+                answer = "wrong pw";
+                break;
+            } else {
+                answer = "fail";
+            }
+        }
+        
+        return answer;
+    }
+}

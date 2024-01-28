b16b551af19f021aa9496e38b1d6a6998cc45262
Soon Choi <135226621+soon91@users.noreply.github.com>
2024-01-28 21:02:51 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.03 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/README.md
+++ 프로그래머스/0/120861. 캐릭터의 좌표/README.md
@@ -1,10 +1,10 @@
 # [level 0] 캐릭터의 좌표 - 120861 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120861#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120861) 
 
 ### 성능 요약
 
-메모리: 83.5 MB, 시간: 0.04 ms
+메모리: 82 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 6일 18:42:57
+2024년 1월 0일 21:2:50
 
 ### 문제 설명
 
--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -3,13 +3,13 @@ class Solution {
         int[] answer = {0, 0};
 
         for (int i = 0; i < keyinput.length; i++) {
-            if (keyinput[i].equals("up") && (answer[1] < 0 || board[1] / 2 > answer[1])) {
+            if (keyinput[i].equals("up") && board[1] / 2 > answer[1]) {
                 answer[1] += 1;
-            } else if (keyinput[i].equals("down") && (answer[1] > 0 || board[1] / 2 > Math.abs(answer[1]))) {
+            } else if (keyinput[i].equals("down") && -board[1] / 2 < answer[1]) {
                 answer[1] += -1;
-            } else if (keyinput[i].equals("left") && (answer[0] > 0 || board[0] / 2 > Math.abs(answer[0]))) {
+            } else if (keyinput[i].equals("left") && -board[0] / 2 < answer[0]) {
                 answer[0] += -1;
-            } else if (keyinput[i].equals("right") && (answer[0] < 0 || board[0] / 2 > answer[0])) {
+            } else if (keyinput[i].equals("right") && board[0] / 2 > answer[0]) {
                 answer[0] += 1;
             }
         }

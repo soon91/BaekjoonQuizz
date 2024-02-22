3bb5792980f63c644052cb4f4b723782e64e0a06
Soon Choi <135226621+soon91@users.noreply.github.com>
2024-02-23 00:54:56 +0900

    [Bronze V] Title: 시험 성적, Time: 208 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+
+        if (A > 89) {
+            System.out.println("A");
+        } else if (A > 79) {
+            System.out.println("B");
+        } else if (A > 69) {
+            System.out.println("C");
+        } else if (A > 59) {
+            System.out.println("D");
+        } else System.out.println("F");
+    }
+}

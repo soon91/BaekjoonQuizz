9aadb6344c67fe9c47549f11bd031c2704b674f4
Soon Choi <135226621+soon91@users.noreply.github.com>
2024-02-28 03:40:09 +0900

    [Bronze IV] Title: 주사위 세개, Time: 204 ms, Memory: 17612 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+
+        if (A == B && B == C) {
+            System.out.println(10000 + (A * 1000));
+        } else if (A == B) {
+            System.out.println(1000 + (A * 100));
+        } else if (A == C) {
+            System.out.println(1000 + (A * 100));
+        } else if (B == C) {
+            System.out.println(1000 + (B * 100));
+        } else if (A > B && A > C) {
+            System.out.println(A*100);
+        } else if (B > A && B > C) {
+            System.out.println(B*100);
+        } else System.out.println(C*100);
+    }
+}

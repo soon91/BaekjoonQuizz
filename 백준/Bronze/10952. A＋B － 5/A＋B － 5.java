4294eb86d2c7186a034c8bacc5b3767a288e6a7f
Soon Choi <135226621+soon91@users.noreply.github.com>
2024-03-10 16:42:50 +0900

    [Bronze V] Title: A+B - 5, Time: 228 ms, Memory: 17924 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; ; i++) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+
+            if (A == 0 && B == 0){
+                break;
+            } else System.out.println(A+B);
+        }
+    }
+}
